46dd720c156e11d9d0ba06b63b086da9bd0e67d4
TLamb23 <lambert.tanner23@gmail.com>
2021-11-11 21:34:31 -0700

    minor changes to the project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from "./ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { projects } from "../../constants/constants";
 
 const Projects = () => (
   <Section nopadding id="projects">
     <SectionDivider />
-    <SectionTitle main>Projects
-  </SectionTitle>
-<GridContainer>
-  {[0, 1, 2, 4, 5].map((project) => (
-
-  <div>
-{project}
-  </div>
-))}
-</GridContainer>
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {[0, 1, 2, 4, 5].map((project) => (
+        <div>{project}</div>
+      ))}
+    </GridContainer>
   </Section>
 );
 
